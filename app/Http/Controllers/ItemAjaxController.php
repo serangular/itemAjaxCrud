feefed66d00dc6f1c101b2071737ce15fa6a91c0
Avanesyan <savanesy@mgc.mentorg.com>
2018-02-20 19:13:48 -0800

    before github

---
+++ app/Http/Controllers/ItemAjaxController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Item;
+// for now starts in    http://localhost/itemAjaxCrud/resources/views/manage-item-ajax.blade.php
+
+class ItemAjaxController extends Controller
+{
+    public function manageItemAjax()
+    {
+        return view('manage-item-ajax');
+    }
+    
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $items = Item::latest()->paginate(5);
+        return response()->json($items);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $create = Item::create($request->all());
+        
+        // debug :  view('manage-item-ajax'); 
+        return response()->json($create);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $edit = Item::find($id)->update($request->all());
+        return response()->json($edit);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Item::find($id)->delete();
+        return response()->json(['done']);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('manage-item-ajax', 'ItemAjaxController@manageItemAjax');
+Route::resource('item-ajax', 'ItemAjaxController');
